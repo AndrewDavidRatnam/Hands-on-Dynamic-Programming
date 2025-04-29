b35c2893fbc19d2a13d872be360e384343e2eb3b
Andrew David Ratnam Sathiadass <155766692+ANdIeCOOl@users.noreply.github.com>
2025-04-29 16:58:58 +0000

    wrong method lol

--- Day 1/Question 2/question2.py
+++ Day 1/Question 2/question2.py
@@ -1,22 +1,35 @@
-min_index_table = []
 
-length = input("Enter number of stones")
-heights = []
-for x in range(length):
-    height = input("Enter the heights")
-    heights.append(height)
-print(f"Heights list: {heights}")
+
+# LENGTH = int(input("Enter number of stones"))
+# heights = []
+heights = [30, 10, 60, 10, 60, 50]
+# for x in range(LENGTH):
+#     height = input("Enter the heights")
+#     heights.append(height)
+# print(f"Heights list: {heights}")
 
 def minCost(heights): #[10, 30, 40, 20]
-    current_stone = heights[0]
+    length = len(heights)
+    min_index_table = [-1 for x in range(length)]
+    for i, height in enumerate(heights):
+        print(i)
+        print(min_index_table)
+        if i == (length - 2 ):
+            min_index_table[i] = i+1
+            break
+        if i == (length - 1 ):
+            break
+        if abs(height - heights[i+1]) == abs(height - heights[i+2]):
+            min_index_table[i] = i+2
+            continue 
+            #expression_if_true if condition else expression_if_false
+        min_index_table[i] = i+1 if (abs(height - heights[i+1]) < abs(height - heights[i+2])) else (i+2)
 
-    min_cost = 0
+    return min_index_table
+        
 
-    min_index = return_min_index(heights[1:])
+print(minCost(heights))
 
-    min_cost = abs(current_stone - heights[min_index])
-
-def return_min_index(heights):#[30, 40, 20]
 
 
 
