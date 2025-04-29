5e10ea0247b1284de5fad8639fb83d44842467f8
Andrew David Ratnam Sathiadass <155766692+ANdIeCOOl@users.noreply.github.com>
2025-04-29 23:11:25 +0530

    Update question2.py

--- Day 1/Question 2/question2.py
+++ Day 1/Question 2/question2.py
@@ -1,32 +1,12 @@
+def minCost(heights):
+  length = len(heights)
+  distance_table = [0 for x in range(length)]
+  distance_table[0] = 0
+  for i in range(length):
+    distance_table[i] = min(distance_table[i-1] + abs(heights[i] - heights[i-1]), distance_table[i-2] + abs(heights[i] - heights[i-2]) if i > 1 else float("inf"))
+  return distance_table[-1]
 
-
-# LENGTH = int(input("Enter number of stones"))
-# heights = []
-heights = [30, 10, 60, 10, 60, 50]
-# for x in range(LENGTH):
-#     height = input("Enter the heights")
-#     heights.append(height)
-# print(f"Heights list: {heights}")
-
-def minCost(heights): #[10, 30, 40, 20]
-    length = len(heights)
-    min_index_table = [-1 for x in range(length)]
-    for i, height in enumerate(heights):
-        print(i)
-        print(min_index_table)
-        if i == (length - 2 ):
-            min_index_table[i] = i+1
-            break
-        if i == (length - 1 ):
-            break
-        if abs(height - heights[i+1]) == abs(height - heights[i+2]):
-            min_index_table[i] = i+2
-            continue 
-            #expression_if_true if condition else expression_if_false
-        min_index_table[i] = i+1 if (abs(height - heights[i+1]) < abs(height - heights[i+2])) else (i+2)
-
-    return min_index_table
-        
+heightss = [[10, 30, 40, 20],[10, 10],[30, 10, 60, 10, 60, 50] ]
 
 print(minCost(heights))
 
